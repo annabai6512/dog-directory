5f64451999a7de077cd66d76903664ca9762c1bf
annabai <anna.bai.6512@gmail.com>
2023-06-21 13:10:14 -0500

    added styling and submitted tracker

--- app/controllers/add.js
+++ app/controllers/add.js
@@ -1,33 +1,49 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class AddController extends Controller {
   @service store;
   @service("dog-database") dogDatabase;
+  @tracked submitted = false;
 
   @action
   save() {
     event.preventDefault();
-    let activities = this.activities ? [...this.activities] : [];
-    let dog = this.store.createRecord('dog', {
-        id: this.dogDatabase.idNum,
-        name: this.name,
-        breed: this.breed,
-        owner: this.owner,
-        size: this.size,
-        description: this.description,
-        meals: {
-          breakfast: this.breakfast,
-          dinner: this.dinner,
-        },
-        activities: activities,
-    });
+    console.log(this.submitted);
+    if (!this.submitted) {
+      let activities = this.activities ? [...this.activities] : [];
+      let dog = this.store.createRecord('dog', {
+          id: this.dogDatabase.idNum,
+          name: this.name,
+          breed: this.breed,
+          owner: this.owner,
+          size: this.size,
+          description: this.description,
+          meals: {
+            breakfast: this.breakfast,
+            dinner: this.dinner,
+          },
+          activities: activities,
+      });
+      
+      this.dogDatabase.addNewDog(dog);
+      this.dogDatabase.idNum += 1;
+      console.log(this.dogDatabase.allDogs);
+      console.log("new id: " + this.dogDatabase.idNum);
+      this.name = null;
+      this.breed = null;
+      this.owner = null;
+      this.size = null;
+      this.description = null;
+      this.meals = null;
+      this.activities = null;
+
+      this.submitted = true;
+      console.log(this.submitted);
+    }
     
-    this.dogDatabase.addNewDog(dog);
-    this.dogDatabase.idNum += 1;
-    console.log(this.dogDatabase.allDogs);
-    console.log("new id: " + this.dogDatabase.idNum);
     // dog.save().then(() => {
     //   console.log('save successful');
     // });
@@ -35,5 +51,10 @@ export default class AddController extends Controller {
     //     console.log('save failed');
     //   }
     // );
+  }
+
+  @action
+  back() {
+    this.submitted = false;
   }
 }
